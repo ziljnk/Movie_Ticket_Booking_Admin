39266f6e7a439ef30a5c39c9fff0825bcfe2dba2
TKhai55 <thienkhaiak5555@gmail.com>
2023-12-04 11:19:44 +0700

    fe 90% manage Theatre

---
+++ src/views/manageTheatre/components/modal-add-theatre/modal-add-theatre.vue
@@ -0,0 +1,42 @@
+<style scope src="./modal-add-theatre.css"></style>
+<script lang="ts" src="./modal-add-theatre.ts"></script>
+
+<template>
+    <div class="modal fade" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" style="min-width:550px">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title w-100" id="ModalLabel">Add New Theatre</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="d-flex flex-column" style="margin:16px;">
+                    <div class="d-flex flex-row" style="gap:32px; margin:16px;">
+                        <div class="col d-flex flex-column" style="gap:16px">
+                            <div class="input-field">
+                                <p class="text-start" style="height:30px; font-weight:600">Name:</p>
+                                <div class="d-flex flex-column" style="gap:4px">
+                                    <input class="input" v-model="movieInput.name" style="height:30px" type="text"
+                                        placeholder="Enter genre's name" />
+                                </div>
+                                
+                            </div>
+                            <div class="input-field">
+                                <p class="text-start" style="height:30px; font-weight:600">Description:</p>
+                                <div class="d-flex flex-column" style="gap:4px">
+                                    <textarea class="text-field input" v-model="movieInput.actors"
+                                        placeholder="Enter theatre's description"></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>  
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="button-outline" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="button-solid">Add</button>
+                </div>
+            </div>
+        </div>
+</div></template>
--- src/views/manageTheatre/manageTheatre.ts
+++ src/views/manageTheatre/manageTheatre.ts
@@ -1,13 +1,26 @@
 import { Vue,Options } from 'vue-class-component'
 import NavigationBar from '@/components/navigationBar/navigationBar.vue'
 import Header from '@/components/header/header.vue'
-
+import ModalAddTheatre from './components/modal-add-theatre/modal-add-theatre.vue';
+import ModalDetailTheatre from './components/modal-detail-theatre/modal-detail-theatre.vue';
 @Options({
     components: {
         NavigationBar,
-        Header
+        Header,
+        ModalAddTheatre,
+        ModalDetailTheatre
     }
 })
 export default class ManageTheatres extends Vue {
-
+    public date:any=null;
+    public searchQuery:any=null;
+    public currentPage:any=1;
+    public totalPage:any=1;
+    public theatres:any=[
+        {
+            id: "6551d62ef6783a532f28440b",
+            name: "Theater 01",
+            description: "The first theatre on the left"
+        }
+    ]
 }
